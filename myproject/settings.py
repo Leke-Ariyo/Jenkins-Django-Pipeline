fd3aa62a8919b4206ed5f1edae7c454d606bf3ba
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2020-07-23 19:35:35 +0100

    Update

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,7 +10,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
