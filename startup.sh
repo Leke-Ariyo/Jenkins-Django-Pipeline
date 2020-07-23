03f2e7097f504a55031977c67b8550e914349346
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2020-07-23 09:24:06 +0100

    Initial Commit

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# python3 -m venv env
+
+# source env/bin/activate
+
+# pip3 install -r requirements.txt 
+
+# cp .env.example .env
+
+python manage.py migrate
+
+python manage.py runserver
